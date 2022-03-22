ca60b91ecb25acd9a1943942fe535f35d5ba7c1d
Hrishikesh Barhate <HrishikeshBarhate@gmail.com>
2022-03-22 22:11:54 +0530

    AA-1 Initial Commit

---
+++ src/main/java/com/shield/AvengersAssembleApplication.java
@@ -0,0 +1,16 @@
+package com.shield;
+
+import java.util.Scanner;
+
+public class AvengersAssembleApplication {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Welcome to AvengersAssemble Game.");
+
+
+	}
+
+}
